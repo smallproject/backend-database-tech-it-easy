75917b7c2060b6d1d0971806e7705f1e38f97d55
smallproject <j.a.villas@live.com.ph>
2024-08-27 10:20:29 +0200

    added create database sql script

---
+++ src/SQL/create-database.sql
@@ -0,0 +1,14 @@
+-- Database: TechItEasy
+
+-- DROP DATABASE IF EXISTS "TechItEasy";
+
+CREATE DATABASE "TechItEasy"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Dutch_Netherlands.1252'
+    LC_CTYPE = 'Dutch_Netherlands.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
