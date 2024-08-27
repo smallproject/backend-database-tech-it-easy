8a63dd1998328db0618f274439eaae29640960fa
smallproject <j.a.villas@live.com.ph>
2024-08-27 11:42:48 +0200

    updated create tables adding constraints and relation

--- src/SQL/create-tables.sql
+++ src/SQL/create-tables.sql
@@ -1,7 +1,35 @@
-DROP TABLE users;
+-- Drop tables if they exist to avoid errors
+DROP TABLE IF EXISTS  users;
+DROP TABLE IF EXISTS cimodule;
+DROP TABLE IF EXISTS wallbracket;
+DROP TABLE IF EXISTS remotecontrollers;
+DROP TABLE IF EXISTS televisions;
 
+-- DROP TABLE IF EXISTS televisions;
+-- Create the televisions table
+CREATE TABLE televisions (
+	Id int PRIMARY KEY,
+	Name varchar(255),
+	Brand varchar(255),
+	Price decimal,
+	Currentstock int,
+	Sold int,
+	Datesold date,
+	Type varchar(255),
+
+	Height decimal(8,2),
+	Width decimal(8,2),
+	SchermKwaliteit varchar(255),
+	Wifi boolean,
+	SmartTv boolean,
+	Voicecontrol boolean,
+	HDR Boolean
+	);
+
+-- DROP TABLE IF EXISTS users;
+-- Create the users table
 CREATE TABLE users (
-	Id int,
+	Id int PRIMARY KEY,
 	Username varchar(255),
 	Password varchar(255),
 	Address varchar(255),
@@ -10,35 +38,13 @@ CREATE TABLE users (
 	VakantieDagen int	
 );
 
-DROP TABLE televisions;
-
-CREATE TABLE televisions (
-	Id int,
-	Name varchar(255),
-	Brand varchar(255),
-	Price decimal,
-	Currentstock int,
-	Sold int,
-	Datesold date,
-	Type varchar(255),
-
-	Height decimal,
-	Width decimal,
-	SchermKwaliteit varchar(255),
-	Wifi boolean,
-	SmartTv boolean,
-	Voicecontrol boolean,
-	HDR Boolean
-	);
-
-
-DROP TABLE remotecontrollers;
-
+-- DROP TABLE IF EXISTS remotecontrollers;
+-- Create the remotecontrollers table
 CREATE TABLE remotecontrollers(
-	Id int,
+	Id int PRIMARY KEY,
 	Name varchar(255),
 	Brand varchar(255),
-	Price decimal,
+	Price decimal(10,2),
 	Currentstock int,
 	Sold int,
 	Datesold date,
@@ -46,13 +52,18 @@ CREATE TABLE remotecontrollers(
 
 
 	Smart boolean,
-	Batterytype varchar(255)
+	Batterytype varchar(255),
+	Televisionid int,-- Foreign key for the relationship
+	
+	-- Define the foreign key constraint
+    FOREIGN KEY (Televisionid) REFERENCES televisions(Id)
 	);
 
-DROP TABLE cimodule;
 
+-- DROP TABLE IF EXISTS cimodule;
+-- Create the cimodule table
 CREATE TABLE cimodule(
-	Id int,
+	Id int PRIMARY KEY,
 	Name varchar(255),
 	Brand varchar(255),
 	Price decimal,
@@ -63,13 +74,18 @@ CREATE TABLE cimodule(
 
 
 	Provider varchar(255),
-	Encoding varchar(255)
+	Encoding varchar(255),
+	Televisionid int,-- Foreign key for the relationship
+	
+	-- Define the foreign key constraint
+    FOREIGN KEY (Televisionid) REFERENCES televisions(Id)
 	);
 
-DROP TABLE wallbracket;
 
+-- DROP TABLE IF EXISTS wallbracket;
+-- Create the wallbracket table
 CREATE TABLE wallbracket(
-	Id int,
+	Id int PRIMARY KEY,
 	Name varchar(255),
 	Brand varchar(255),
 	Price decimal,
@@ -80,7 +96,12 @@ CREATE TABLE wallbracket(
 
 
 	Adjustable boolean,
-	Bevestigingsmethode varchar(255)--,
-	Height decimal,
-	Width decimal
+	Bevestigingsmethode varchar(255),
+	Height decimal(8,2),
+	Width decimal(8,2),
+	Televisionid int,-- Foreign key for the relationship
+	
+	-- Define the foreign key constraint
+    FOREIGN KEY (Televisionid) REFERENCES televisions(Id)
 	);
+
